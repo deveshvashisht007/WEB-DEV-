cf3c3bd87347c94c180e44fa29aa463ac9913b55
deveshvashisht007 <deveshvashisht176@gmail.com>
2026-02-09 21:13:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,127 @@
+
+//const para=document.querySelectorAll(".para")
+//para[0].textContent="this is a paragraph"
+//para[1].style.color="green"
+//console.log(para)
+
+//const button=document.querySelector("button")
+//button.classList.add("btn")
+//button.classList.add("btn2")
+//button.classList.remove("btn2")
+
+//function show(){
+    //alert("you have clicked the button")
+//}
+
+//button.addEventListener("click",show)
+//button.addEventListener("clcik",show)
+//index.html
+//<button id ="btn">click me</button>
+//<button id="stop">stop</button>
+//script.js
+
+
+//const button=document.getElementById("btn")
+//const stopbtn=document.getElementById("stop")
+
+
+//function show(){
+    //alert("you have clicked the button")
+//}
+
+//button.addEventListener("click",show)
+//button.addEventListener("clcik",show)
+//index.html
+//<button id ="btn">click me</button>
+//<button id="stop">stop</button>
+//script.js
+
+
+//const button=document.getElementById("btn")
+//const stopbtn=document.getElementById("stop")
+//});
+
+
+//const btn=document.querySelector("#btn")
+//const stopbtn=document.querySelector("#stop")
+
+//btn.addEventListener("keyup",(event)=>{
+//    console.log(event.key)
+//})
+
+//const form=document.querySelector("form")
+//form.addEventListener("submit",(event)=>{
+//    event.preventDefault()
+//    console.log(event.srcElement[0].value)
+//    console.log(event.srcElement[1].value)
+//    console.log(event)
+    //console.log("button clciked")
+//})
+//const form=document.querySelector("form")
+//form.addEventListener("submit",(event)=>{
+//    event.preventDefault()
+//    alert("form submitted")
+//})
+
+//debugger
+//console.log(a);
+
+//var a = 3453
+//let b = 456;
+
+//console.log(a)
+//console.log(b);
+
+//function print(){
+    //console.log("xyz");
+//}
+
+//print()
+
+
+//console.log("starting homewoek..");
+
+//setTimeout(() => {
+    //console.log("homework done!");
+    //console.log("staring dinner..");
+
+    //setTimeout(() => {
+//         console.log("dinner done!");
+//         console.log("getting ready to go out..");
+
+//         setTimeout(() => {
+//             console.log("going to the playground!");
+//         }, 1000);// after dinner
+
+//     }, 2000);// dinner time
+
+// }, 3000);// homework time
+
+
+// function finishedHomework(){
+//     console.log("starting homework...");
+//     setTimeout(() => {
+//         console.log("homework done !");
+//         callback();
+//     },2000);
+// }
+
+// PROMISES
+//  const p = new Promise((res,rej))=>{
+//     console.log("going to do the homework!");
+//     setTimeout(() => {
+//         const done = true;
+//         if(done){
+//             res("success");
+//     }else{
+//         rej("failed to fetch data from the server");
+//      }
+//         },3000)
+
+//      }
+
+Form.addeventlistener("submit",(event)=>{
+    const category=eventcategory.value
+    const description=eventdescription.value
+    console.log(title,Date,category,description)
+    const card =document
